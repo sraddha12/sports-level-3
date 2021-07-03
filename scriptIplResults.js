f17d81dfd74115303265e4cf806230c4d5bf1de7
sraddha12 <77789398+sraddha12@users.noreply.github.com>
2021-07-03 19:27:41 +0530

    Add files via upload

---
+++ scriptIplResults.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyChtuGMP3KzZBP0DhAjOprGfn5CNurRyn4",
+    authDomain: "iplresults-9ef77.firebaseapp.com",
+    databaseURL: "https://iplresults-9ef77.firebaseio.com",
+    projectId: "iplresults-9ef77",
+    storageBucket: "iplresults-9ef77.appspot.com",
+    messagingSenderId: "533733582731",
+    appId: "1:533733582731:web:c5c9f9d7e2f46c3767318e"
+  };
+  firebase.initializeApp(firebaseConfig);
+
